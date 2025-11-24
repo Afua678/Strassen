4920dfbd02abc6056ee53c799e5052796347117c
Afua678 <afua.anyadike@gmail.com>
2025-11-24 08:41:31 -0500

    made random strassen matric

--- MatrixMult.java
+++ MatrixMult.java
@@ -5,7 +5,22 @@ import java.util.Scanner;
 public class MatrixMult {
     public static void main(String[] args) {
         System.out.println("What is your n?");
-        int myInteger = scanner.nextInt();
+        int num = scanner.nextInt();
+        System.out.println("Starting experiment for n = " + num);
+
+        int[][] strass = makeMatrix(num);
+    }
+
+    public static int[][] makeMatrix(int n) {
+        int[][] strass = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                strass[i][j] = (int) Math.random() * 10;
+            }
+
+        }
+
+        return strass;
     }
 
 }
