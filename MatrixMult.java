8d0e8e03e538ebcb782c67668af6d53f914490fd
Afua678 <afua.anyadike@gmail.com>
2025-11-24 08:28:04 -0500

    takes inputs

--- MatrixMult.java
+++ MatrixMult.java
@@ -1,5 +1,11 @@
 package Strassen;
 
+import java.util.Scanner;
+
 public class MatrixMult {
+    public static void main(String[] args) {
+        System.out.println("What is your n?");
+        int myInteger = scanner.nextInt();
+    }
 
 }
