3ea267d2cedee5b5df98a38089ab4c50c782646f
BellaLGuo <bellalguo@gmail.com>
2025-11-30 21:09:18 -0500

    Completed Strassen

--- MatrixMult.java
+++ MatrixMult.java
@@ -50,18 +50,28 @@ public class MatrixMult {
         int[][] b10 = splitBottomLeft(B);
         int[][] b11 = splitBottomRight(B);
 
-
-
-        mult(a);
-        mult(b);
-        mult(c);
-        mult(d); // do the methods
+        // Recursively computing m values
+        int[][] m1 = strassen(add(a00, a11), add(b00, b11));
+        int[][] m2 = strassen(add(a10, a11), b00);
+        int[][] m3 = strassen(a00, sub(b01,b11));
+        int[][] m4 = strassen(a11, sub(b10, b00));
+        int[][] m5 = strassen(add(a00, a01), b11);
+        int[][] m6 = strassen(sub(a10, a00), add(b00, b01));
+        int[][] m7 = strassen(sub(a01, a11), add(b10, b11));
         
+        // Combining m values for final product matrix C
+        int[][] c00 = add(m1,add(m7, sub(m4, m5)));
+        int[][] c01 = add(m3, m5);
+        int[][] c10 = add(m2, m4);
+        int[][] c11 = add(m1, add(m6, sub(m3, m2)));
+        int[][] C = join(c00,c01, c10, c11);
+        
+        // Method ends after obtaining C
         long end = System.currentTimeMillis();
         return (double)(end - start / 1000);
     }
     
-    public static int[][] splitTopLeft(int[][] s) {
+    private static int[][] splitTopLeft(int[][] s) {
         int[][] n = new int[s.length / 2][s.length / 2];
 
         for (int i = 0; i < n.length; i++) {
@@ -74,7 +84,7 @@ public class MatrixMult {
 
     }
 
-    public static int[][] splitTopRight(int[][] s) {
+    private static int[][] splitTopRight(int[][] s) {
         int[][] n = new int[s.length / 2][s.length / 2];
 
         for (int i = 0; i < n.length; i++) {
@@ -87,7 +97,7 @@ public class MatrixMult {
 
     }
 
-    public static int[][] splitBottomLeft(int[][] s) {
+    private static int[][] splitBottomLeft(int[][] s) {
         int[][] n = new int[s.length / 2][s.length / 2];
 
         for (int i = 0; i < n.length; i++) {
@@ -100,7 +110,7 @@ public class MatrixMult {
         return n;
     }
 
-    public static int[][] splitBottomRight(int[][] s) {
+    private static int[][] splitBottomRight(int[][] s) {
         int[][] n = new int[s.length / 2][s.length / 2];
 
         for (int i = 0; i < n.length; i++) {
@@ -130,5 +140,20 @@ public class MatrixMult {
         return C;
     }
 
-    
+    private static int[][] join(int[][] c00, int[][] c01, int[][] c10, int[][] c11) {
+        int n = c00.length;
+        int[][] C = new int[2*n][2*n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = c00[i][j];
+                C[i][n+j] = c01[i][j];
+                C[n+i][j] = c10[i][j];
+                C[n+i][n+j] = c11[i][j];
+            }
+        }
+
+        return C;
+    }
+
 }
